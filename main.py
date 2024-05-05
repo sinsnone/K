3864f359d9ed9efe8cc87e7c5133cfbe0b022386
QORSAN <139044396+SINSQQ@users.noreply.github.com>
2024-05-05 02:21:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
 import config
 
-os.system(f'git clone https://{config.e}@github.com/{config.ee}/{config.eee} && python3 {config.eee}/main.py')
+os.system(f'git clone https://{config.e}@github.com/{config.ee}/{config.eee} && cd {config.eee} && python3 main.py')
